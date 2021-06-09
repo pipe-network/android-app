ecbd44d23913f268a68d0f4196fb74c4f45d207b
Michael Bykovski <michael.bykovskii@aoe.com>
2021-06-09 12:21:30 +0200

    Update PurgeService with other models

--- app/src/main/java/com/pipe_network/app/application/services/PurgeService.kt
+++ app/src/main/java/com/pipe_network/app/application/services/PurgeService.kt
@@ -1,6 +1,8 @@
 package com.pipe_network.app.application.services
 
 import androidx.annotation.WorkerThread
+import com.pipe_network.app.application.repositories.FeedRepository
+import com.pipe_network.app.application.repositories.ForeignFeedRepository
 import com.pipe_network.app.application.repositories.FriendRepository
 import com.pipe_network.app.application.repositories.ProfileRepository
 import javax.inject.Inject
@@ -12,6 +14,8 @@ interface PurgeService {
 class PurgeServiceImpl @Inject constructor(
     private val profileRepository: ProfileRepository,
     private val friendRepository: FriendRepository,
+    private val feedRepository: FeedRepository,
+    private val foreignFeedRepository: ForeignFeedRepository,
 ) : PurgeService {
 
     @WorkerThread
@@ -19,5 +23,11 @@ class PurgeServiceImpl @Inject constructor(
         profileRepository.delete()
         val friends = friendRepository.all()
         friendRepository.delete(*friends.toTypedArray())
+
+        val feeds = feedRepository.all()
+        feedRepository.delete(*feeds.toTypedArray())
+
+        val foreignFeeds = foreignFeedRepository.all()
+        foreignFeedRepository.delete(*foreignFeeds.toTypedArray())
     }
 }
